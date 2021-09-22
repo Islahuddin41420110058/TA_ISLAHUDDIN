847979f171f6b19168ec7f6b54ffb444b946fbbe
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-09-22 20:50:34 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -11,11 +11,6 @@ const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]). red
 const argMax = argFact((min, el) => (el[0] > min[0] ? el : min ))
 
 function ArgMax(res){
-  label = "NORMAL"
-    cls_data = []
-    for(i=0; i<res.length; i++){
-        cls_data[i] = res[i]
-    }
     console.log(cls_data, argMax(cls_data));
     
   if(argMax(cls_data) == 1){
