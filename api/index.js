a9b772e98e8746535be1a59c43e65cbdfa247d7d
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-10-18 21:32:23 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
+const model = require('./sdk/model.js'); //predict
 const cls_model = require('./sdk/cls_model.js'); // cls
 
 // Bot Setting
@@ -15,16 +16,16 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /classify`
+        click /predict`
     );   
     state = 0;
 });
 
 // input requires i and r
-bot.onText(/\/classify/, (msg) => { 
+bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `masukan nilai suhu|kelembaban contohnya 30|700`
+        `masukan nilai suhu|kelembaban contohnya 30|200`
     );   
     state = 1;
 });
@@ -32,23 +33,27 @@ bot.onText(/\/classify/, (msg) => {
 bot.on('message', (msg) => {
     if(state == 1){
         s = msg.text.split("|");
-        cls_model.classify(
+        model.predict(
             [
                 parseFloat(s[0]), // string to float
                 parseFloat(s[1])
             ]
         ).then((jres1)=>{
           console.log(jres1);
-            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0])]).then((jres2)=>{
-            bot.sendMessage(
-                msg.chat.id,
-                `Keadaan pompa yang diprediksi adalah ${jres1[0]}`
-            ); 
             
+            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres[1])]).then((jres2)=>{
             bot.sendMessage(
                 msg.chat.id,
-                `Klasifikasi keadaan pompa ${jres1}`
+                `Keadaan pompa yang diprediksi adalah ${jres1[0]} 1 = pompa on, 0 = pompa off`
             ); 
+            bot.sendMessage(
+                msg.chat.id,
+                `Keadaan kipas yang diprediksi adalah ${jres1[0]} 1 = kipas on, 0 = kipas off`
+            ); 
+            bot.sendMessage(
+                msg.chat.id,
+                `Klasifikasi ${jres2}`
+            );     
             state = 0;
           })
        })
@@ -62,30 +67,31 @@ bot.on('message', (msg) => {
 })
 
 // routers
-r.get('/classify/:suhu/:kelembaban', function(req, res, next) {    
-   cls_model.classify(
+r.get('/predict/:S/:K', function(req, res, next) {    
+   model.predict(
         [
-            parseFloat(req.params.suhu), // string to float
-            parseFloat(req.params.kelembaban)
+            parseFloat(req.params.S), // string to float
+            parseFloat(req.params.K)
         ]     
    ).then((jres)=>{
         res.json(jres);
    })
 });
 
-r.get('/classify/:suhu/:kelembaban', function(req, res, next) {    
-   cls_model.classify(
-           [
-                parseFloat(req.params.suhu), // string to float
-                parseFloat(req.params.kelembaban)
-           ]
+r.get('/classify/:S/:K', function(req, res, next) {    
+   model.predict(
+        [
+             parseFloat(req.params.S), // string to float
+             parseFloat(req.params.K)
+        ]
            
    ).then((jres)=>{
        cls_model.classify(
            [
-                parseFloat(req.params.suhu), // string to float
-                parseFloat(req.params.kelembaban),
-                parseFloat(jres[0])
+                parseFloat(req.params.S), // string to float
+                parseFloat(req.params.K),
+                parseFloat(jres[0]),
+                parseFloat(jres[1])
            ]   
         ).then((jres_)=>{
            res.json({jres, jres_})
